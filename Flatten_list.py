9137730426664fc1ee28c46ceb225209cf888675
MrPuxe <anthondeleonsketch@gmail.com>
2024-02-22 10:59:49 -0500

    Function with instructionss

--- Flatten_list.py
+++ Flatten_list.py
@@ -1,2 +1,8 @@
 # Function takes a list of lists and falttens iit into a 
 # one-dimensional list.
+
+def flatten(a):
+    l = [a[j][i] for j in range(len(a)) for i in range(len(a))]
+    return l
+
+print(flatten([[1,2],[3,4]])) # [1, 2, 3, 4]
